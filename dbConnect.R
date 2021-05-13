c1b863ac4ffec4f360d79995213a1e09fcf63192
mkaranja <m.karanja@cgiar.org>
2021-05-13 12:01:35 +0300

    Update dbConnect.R

--- dbConnect.R
+++ dbConnect.R
@@ -3,9 +3,9 @@ suppressPackageStartupMessages(library(odbc, warn.conflicts=FALSE))
 
 pool <- dbConnect(odbc(),
                   Driver = "ODBC Driver 17 for SQL Server",
-                  Server = "52.212.46.173\\SQLEXPRESS,41433",
-                  Database = "BTRACT",
-                  UID = "mkaranja",
-                  PWD = "a1s2d3"
+                  Server = "****",
+                  Database = "***",
+                  UID = "***",
+                  PWD = "***"
 )
 
